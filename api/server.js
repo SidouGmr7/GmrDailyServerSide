291afe51b3cca88bf6657e6d2ffff8ce2306318b
SidouGmr7 <goumirisidali@hotmail.com>
2023-12-12 12:08:54 +0100

    add headers cors

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const nodeRoutes = require('../routes/nodeRoutes')
 const checkBoxRoutes = require('../routes/checkBoxRoutes')
 const app = express()
 const connection = require('../db')
-const cors = require('cors')
+// const cors = require('cors')
 
 connection()
 
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  
+
     // Intercepts OPTIONS method
     if (req.method === 'OPTIONS') {
       res.sendStatus(200);
@@ -20,10 +20,9 @@ app.use((req, res, next) => {
     }
   });
 
-if (process.env.NODE_ENV === 'development') {
-    app.listen(3001)
-    app.use(cors())
-}
+app.listen(3001)
+// app.use(cors())
+
 app.use(express.json())
 
 app.use('/api/node', nodeRoutes)
