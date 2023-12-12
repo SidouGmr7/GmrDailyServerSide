66cf3aa6858a51c6da56ce7c2e8c7dc558e25953
SidouGmr7 <goumirisidali@hotmail.com>
2023-12-12 11:23:18 +0100

    test in vercel

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const nodeRoutes = require('./routes/nodeRoutes')
 const checkBoxRoutes = require('./routes/checkBoxRoutes')
 const app = express()
 const connection = require('./db')
-const cors = require('cors')
 
 connection()
 
-app.listen(3001)
 app.use(express.json())
-app.use(cors())
 
 app.use('/api/node', nodeRoutes)
 app.use('/api/checkbox', checkBoxRoutes)
+
+module.exports = app
