d489321382f79f9381c482c36dd86cb2be7d474d
SidouGmr7 <goumirisidali@hotmail.com>
2023-12-12 17:16:15 +0100

    db change

--- api/server.js
+++ api/server.js
@@ -4,21 +4,9 @@ const checkBoxRoutes = require('../routes/checkBoxRoutes')
 const app = express()
 const connection = require('../db')
 const cors = require('cors')
+require('dotenv').config();
 
 connection()
-
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-//     // Intercepts OPTIONS method
-//     if (req.method === 'OPTIONS') {
-//       res.sendStatus(200);
-//     } else {
-//       next();
-//     }
-//   });
 
 app.listen(3001)
 app.use(cors())
